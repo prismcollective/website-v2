6184ddde62cff1b76cbc5ec9c1a9b75e3e87346f
Maisha Thasin <thasin.maisha@gmail.com>
2025-01-25 01:36:02 -0500

    added resend

--- src/app/api/send-email/route.ts
+++ src/app/api/send-email/route.ts
@@ -8,12 +8,12 @@ export async function POST(req: Request) {
     const { fullName, email, message } = await req.json();
 
     const { data, error } = await resend.emails.send({
-      from: "UW PRISM Website <inquiry@uwprism.dev>", // Update this with our domain
+      from: "PRISM <onboarding@resend.dev>", // Update this with our domain
       to: ["uwprism@gmail.com"],
       subject: `New Inquiry from ${fullName}`,
       replyTo: email,
       html: `
-        <h2>New Inquiry</h2>
+        <h2>New Inquiry from website</h2>
         <p><strong>Name:</strong> ${fullName}</p>
         <p><strong>Email:</strong> ${email}</p>
         <h3>Message:</h3>
