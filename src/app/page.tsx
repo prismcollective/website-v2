bf9cfed7db539f5645b60a29a8444cc165e02fa9
Maisha Thasin <thasin.maisha@gmail.com>
2025-01-26 15:23:04 -0500

    added pics

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -6,8 +6,7 @@ import Link from "next/link";
 import Socials from "@/components/Socials";
 import Button from "@/components/Button";
 import Footer from "@/components/Footer";
-
-
+import Image from "next/image";
 
 export default function Home() {
   const [isMobile, setIsMobile] = useState(false);
@@ -40,12 +39,15 @@ export default function Home() {
             PRISM IS AN INTERDISCIPLINARY DESIGN TEAM TO CHALLENGE THE STATUS QUO AND ENCOURAGE NEW FORMS OF EXPRESSION
           </div>
 
-          {/* First White Block */}
-          <div className="h-[300px] border-b-2 border-black">
-
+          <div className="h-[500px] border-b-2 border-black relative">
+            <Image
+              src="/mind-garden-cad.webp"
+              alt="Project showcase 1"
+              fill
+              className="object-cover w-full h-full"
+            />
           </div>
 
-          {/* Blue Section */}
           <div className="bg-[#A3D5F8] p-6 border-b-2 border-black">
             <div className="text-sm mb-4">
               KINETIC SCULPTURES / GENERATIVE ART / PROJECTION MAPPING / BIO-ART / IMMERSIVE EXPERIENCES / AUGMENTED/VIRTUAL REALTIY / ROBOTS / WEARABLE TECHNOLOGY / ANYTHING YOU CAN DREAM OF
@@ -57,21 +59,22 @@ export default function Home() {
             </Link>
           </div>
 
-          {/* Second White Block */}
-          <div className="h-[300px] border-b-2 border-black">
+          <div className="h-[400px] border-b-2 border-black relative">
+            <Image
+              src="/sculpture-mind-garden.webp"
+              alt="Project showcase 2"
+              fill
+              className="object-cover w-full h-full"
+            />
           </div>
 
           <Banner />
-         <Footer />
-
-          {/* Banner */}
-          <Banner />
+          <Footer />
         </div>
       </div>
     );
   }
 
-  // Desktop Layout (Unchanged)
   return (
     <div className="flex flex-col flex-1">
       <div className="flex-1 flex">
@@ -83,7 +86,14 @@ export default function Home() {
         </div>
         <div className="flex flex-1">
           <div className="flex flex-col flex-1 border-r-2 border-black">
-            <div className="flex-1 border-b-2 border-black"></div>
+            <div className="flex-1 border-b-2 border-black relative w-full">
+              <Image
+                src="/sculpture-mind-garden.webp"
+                alt="Project showcase 1"
+                fill
+                className="object-cover w-full h-full"
+              />
+            </div>
             <div className="flex-1 flex flex-col gap-5 items-center justify-center p-6 border-b-2 border-black bg-[#A3D5F8]">
               <div>
                 KINETIC SCULPTURES / GENERATIVE ART / PROJECTION MAPPING / BIO-ART / IMMERSIVE EXPERIENCES / AUGMENTED/VIRTUAL REALTIY / ROBOTS / WEARABLE TECHNOLOGY / ANYTHING YOU CAN DREAM OF
@@ -94,16 +104,32 @@ export default function Home() {
                 </div>
               </Link>
             </div>
-            <div className="flex-1"></div>
+            <div className="flex-1 relative w-full">
+              <Image
+                src="/jellyfish-sculpture.png"
+                alt="Project showcase 2"
+                fill
+                className="object-cover w-full h-full"
+              />
+            </div>
           </div>
           <div className="flex flex-col flex-1">
-            <div className="bg-[#FEE4D4] flex flex-1 border-b-2 border-black items-center justify-center p-6">
+            <div className="h-1/3 bg-[#FEE4D4] flex border-b-2 border-black items-center justify-center p-6">
               PRISM IS AN INTERDISCIPLINARY DESIGN TEAM TO CHALLENGE THE STATUS QUO AND ENCOURAGE NEW FORMS OF EXPRESSION
             </div>
-            <div className="flex-1 border-b-2 border-black flex items-center justify-center p-6"></div>
-            <div className="bg-[#DFF9F6] flex-1 flex flex-col gap-5 items-left justify-center p-6">
-              <div className="font-bold">CONNECT WITH US</div>
-              <Socials />
+            <div className="h-[500px] border-b-2 border-black relative w-full">
+              <Image
+                src="/mind-garden-cad.webp"
+                alt="Project showcase 3"
+                fill
+                className="object-cover w-full h-full"
+              />
+            </div>
+            <div className="flex-1 flex flex-col">
+              <div className="bg-[#DFF9F6] flex-1 flex flex-col gap-5 items-left justify-center p-6">
+                <div className="font-bold">CONNECT WITH US</div>
+                <Socials />
+              </div>
             </div>
           </div>
         </div>
